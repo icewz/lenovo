6061712afee5d9bbccdcb19f37032abb443505a7
icewz <2193440599@qq.com>
2021-06-23 08:25:48 +0800

    clean code notes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lenovo
-111
+
+2021-6-9 23:33:15
+
+From now on, I can use github to submit code on different devices
+Hope I can stick to it.
+
--- javaBase/src/main/java/apiNeedToReadCareFully/IO/test.java
+++ javaBase/src/main/java/apiNeedToReadCareFully/IO/test.java
@@ -1,10 +1,17 @@
 package apiNeedToReadCareFully.IO;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        method1();
+        //method1();
+        GuessStatisticsMessage gsm = new GuessStatisticsMessage();
+        String me = gsm.make('b',4);
+        System.out.println(me);
     }
 
     /**
@@ -20,5 +27,19 @@ public class test {
         input.read(b);
         input.close();
         System.out.println("q:" + new String(b));
+    }
+
+//    public List<Cell> getFlaggedCellls() {
+//        List<Cell> flaggedCells = new ArrayList<Cell>();
+//        for (Cell cell : gameBoard)
+//            if (cell.isFlagged())
+//                flaggedCells.add(cell);
+//        return flaggedCells;
+//    }
+
+    public static void copyChars(char source[], char destination[]) {
+        for (int i = 0; i < source.length; i++) {
+            destination[i] = source[i];
+        }
     }
 }
